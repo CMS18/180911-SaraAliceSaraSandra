5db5c12c3b0021986422e80713c8b96aca6b0ccd
sara <sara.berling@hotmail.com>
2018-09-11 14:41:03 +0200

    Klart sista

--- Story/Story/Program.cs
+++ Story/Story/Program.cs
@@ -14,18 +14,14 @@ namespace Story
             string occasion;
             string belovedOne;
             string danger;
-            
+            string balloon;
             Console.WriteLine("Hej! Skriv in din historia: ");
             Console.Write("Namn: ");
-            string name = Console.ReadLine().ToLower();
+            string name = Console.ReadLine();
             Console.Write("Type: ");
             string art = Console.ReadLine().ToLower();
             Console.Write("Mood: ");
             string mood = Console.ReadLine().ToLower();
-            
-
-
-
             string nameFriend;
             string artFriend;
             string moodFriend;
@@ -44,18 +40,13 @@ namespace Story
             Console.WriteLine(" fell on the poor " + name + ", while " + belovedOne + " was screaming!");
             Console.WriteLine("Talking about an unforgettable " + occasion + ", uh?");
 
-
-            
-
-            
-
             Console.WriteLine($"{name} var så {mood} att {name} ville äta något gott. " +
                 $"Eftersom {name} är en {art} så är det bästa {name} vet pyttipanna.");
             Console.WriteLine($"Trots att {name} var så {mood} så satte {name} igång med att laga " +
                 $"pyttipannan. \nDet var supergott och {name} blev nöjd och glad!");     
 
             Console.WriteLine("Den unika arten såg plötsligt en rosa ballong, ballongen presentera sig som:");
-            string balloon = Console.ReadLine();
+            balloon = Console.ReadLine();
             Console.Write(", " + balloon + " blev förvånad över hur söt den " + art + " var.");
             Console.Write("Han var en VÄLDIGT " + mood + " " + art + " som levde djupt nere i havet tillsammans " +
                 "med sin bästis: ");
