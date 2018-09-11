83cb5160e673eb3c2404172294fdecb06cdeefba
sara <sara.berling@hotmail.com>
2018-09-11 13:55:39 +0200

    La till variabler och text

--- Story/Story/Program.cs
+++ Story/Story/Program.cs
@@ -10,8 +10,18 @@ namespace Story
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hej! Skriv in din historia: ");
+            Console.Write("Namn: ");
+            string name = Console.ReadLine().ToLower();
+            Console.Write("Art: ");
+            string art = Console.ReadLine().ToLower();
+            Console.Write("Mood: ");
+            string mood = Console.ReadLine().ToLower();
+
             Console.WriteLine("Once upon a time...");
+            Console.WriteLine("var det en " + mood + " " + art + " som hette " + name + ". ");
+            
 
             Console.ReadLine();
         }
