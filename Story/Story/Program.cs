97125269a0f3bd693e4dd2728e541ba86f3d1afc
Sara Björnlund <sara_bjornlund@hotmail.com>
2018-09-11 13:39:47 +0200

    Här är början på historian

---
+++ Story/Story/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Story
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej! Skriv in din historia: ");
+            Console.ReadLine();
+        }
+    }
+}
